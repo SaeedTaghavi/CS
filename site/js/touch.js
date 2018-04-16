d6160240e5b8a1b2521b298c70e43bce6b9129f7
Jay <walkccray@gmail.com>
2018-04-17 06:37:47 +0800

    update master

--- site/js/touch.js
+++ site/js/touch.js
@@ -1,6 +1,6 @@
-var clickEvent = (function() {
-    if ('ontouchstart' in document.documentElement === true)
-        return 'touchstart';
-    else
-        return 'click';
-})();
+// var clickEvent = (function() {
+//     if ('ontouchstart' in document.documentElement === true)
+//         return 'touchstart';
+//     else
+//         return 'click';
+// })();
